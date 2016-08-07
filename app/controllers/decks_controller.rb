02724b1c85655788184a23ca18c013e6da98091f
Evon <fumblies@gmail.com>
2016-08-08 01:43:06 +0800

    ability to CRUD decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,8 @@
 class DecksController < ApplicationController
+  before_action :find_deck, only: [:show, :edit, :update, :destroy]
+
   def index
+    @decks = Deck.all.order("created_at DESC")
   end
 
   def new
@@ -9,6 +12,31 @@ class DecksController < ApplicationController
 
   def create
     @deck = Deck.new(deck_params)
+
+    if @deck.save
+      redirect_to @deck, notice: "Successfully created new Deck"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @deck.update(deck_params)
+      redirect_to @deck, notice: "Deck was Successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @deck.destroy
+    redirect_to root_path
   end
 
   private
@@ -16,4 +44,9 @@ class DecksController < ApplicationController
   def deck_params
     params.require(:deck).permit(:title, :description, :isPrivate)
   end
+
+  def find_deck
+	  @deck = Deck.find(params[:id])
+  end
+
 end
